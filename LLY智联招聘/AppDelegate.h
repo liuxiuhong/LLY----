dd975664b08f2d7ab439f455397f37f435ee4fdf
liuxiuhong <liuxiuhong1988@126.com>
2013-02-18 16:37:24 +0800

    Initial Commit

---
+++ LLY智联招聘/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LLY智联招聘
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
